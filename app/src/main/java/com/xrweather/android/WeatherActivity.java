e309de322e685c48d0d434a75dafc545e4e60779
Xr <h759233960@163.com>
2017-03-06 22:26:21 +0800

    增加后台自动更新，上个bug还未修复

--- app/src/main/java/com/xrweather/android/WeatherActivity.java
+++ app/src/main/java/com/xrweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.xrweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.xrweather.android.gson.DailyForecast;
 import com.xrweather.android.gson.Weather;
+import com.xrweather.android.service.AutoUpdateService;
 import com.xrweather.android.util.HttpUtil;
 import com.xrweather.android.util.Utility;
 
@@ -150,6 +152,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
