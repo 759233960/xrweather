30d43952ce067c27401f0c9546294e2f92cc1daa
Xr <h759233960@163.com>
2017-03-06 11:53:58 +0800

    First commit/

---
+++ app/src/main/java/com/xrweather/android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xrweather.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//https://github.com/759233960/xrweather.git
